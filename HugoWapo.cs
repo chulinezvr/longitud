8643efb75d58dbc01c29c61bfc45d7c3ac6838a9
chulinezvr <jsergionioz@gmail.com>
2025-01-14 20:18:32 +0100

    Add files via upload

---
+++ HugoWapo.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HugoWapo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("coco seco 83");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
